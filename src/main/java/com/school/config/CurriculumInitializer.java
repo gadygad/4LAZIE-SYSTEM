36960d2268413d861cdf90fd0c8ba7000246a08a
gadygady <kilingepazasauti@gmail.com>
2026-06-17 18:27:14 +0300

    Add diploma level 5 sem 2 subjects to DIP_IT as well

--- src/main/java/com/school/config/CurriculumInitializer.java
+++ src/main/java/com/school/config/CurriculumInitializer.java
@@ -27,9 +27,9 @@ public class CurriculumInitializer {
                 diplomaCSE = courses.get(0);
             }
 
-            // Check if subjects for level 5 semester 2 exist
-            List<Subject> existingSubjects = subjectRepository.findByCourseIdAndLevelNoAndSemesterNo(diplomaCSE.getId(), 5, 2);
-            if (existingSubjects.isEmpty()) {
+            // Check if subjects for level 5 semester 2 exist for DIP_CSE
+            List<Subject> existingSubjectsCSE = subjectRepository.findByCourseIdAndLevelNoAndSemesterNo(diplomaCSE.getId(), 5, 2);
+            if (existingSubjectsCSE.isEmpty()) {
                 List<String> moduleNames = Arrays.asList(
                         "SERVER ADMINISTRATION",
                         "COMPUTER NETWORKS",
@@ -44,6 +44,37 @@ public class CurriculumInitializer {
                     subject.setSemesterNo(2);
                     subject.setLevelNo(5);
                     subject.setCourse(diplomaCSE);
+                    subject.setCode("DIP" + (520 + moduleNames.indexOf(name) + 1));
+                    subjectRepository.save(subject);
+                }
+            }
+            
+            // Do the same for DIP_IT since it's the default program
+            List<Course> coursesIT = courseRepository.findByProgramType("DIP_IT");
+            Course diplomaIT;
+            if (coursesIT.isEmpty()) {
+                diplomaIT = new Course("DIPLOMA IN IT", "DIP_IT");
+                diplomaIT = courseRepository.save(diplomaIT);
+            } else {
+                diplomaIT = coursesIT.get(0);
+            }
+            
+            List<Subject> existingSubjectsIT = subjectRepository.findByCourseIdAndLevelNoAndSemesterNo(diplomaIT.getId(), 5, 2);
+            if (existingSubjectsIT.isEmpty()) {
+                List<String> moduleNames = Arrays.asList(
+                        "SERVER ADMINISTRATION",
+                        "COMPUTER NETWORKS",
+                        "COMPUTER ARCHITECTURE AND ASSEMBLY PROGRAMMING LANGUAGE",
+                        "WEB DESIGNING",
+                        "BASIC DATA COMMUNICATION"
+                );
+
+                for (String name : moduleNames) {
+                    Subject subject = new Subject();
+                    subject.setName(name);
+                    subject.setSemesterNo(2);
+                    subject.setLevelNo(5);
+                    subject.setCourse(diplomaIT);
                     subject.setCode("DIP" + (520 + moduleNames.indexOf(name) + 1));
                     subjectRepository.save(subject);
                 }
