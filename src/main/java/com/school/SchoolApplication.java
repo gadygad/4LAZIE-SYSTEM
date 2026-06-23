9891bc0b09082e31bf3dfb89cded0f741737c6d8
gadygady <kilingepazasauti@gmail.com>
2026-06-23 08:07:28 +0300

    Remove wipe script

--- src/main/java/com/school/SchoolApplication.java
+++ src/main/java/com/school/SchoolApplication.java
@@ -2,22 +2,10 @@ package com.school;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import com.school.repository.NoteRepository;
 
 @SpringBootApplication
 public class SchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner wipeAllNotes(NoteRepository noteRepository) {
-        return args -> {
-            long count = noteRepository.count();
-            noteRepository.deleteAll();
-            System.out.println("========== WIPED " + count + " NOTES ==========");
-        };
     }
 }
