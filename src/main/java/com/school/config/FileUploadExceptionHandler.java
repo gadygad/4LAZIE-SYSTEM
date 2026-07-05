7ab1feeb2ab19f37ecaa8e87a8c7b8c9ad695dd3
gadygady <kilingepazasauti@gmail.com>
2026-07-05 14:04:16 +0300

    Rename config GlobalExceptionHandler to FileUploadExceptionHandler to actually fix the bean name conflict since ControllerAdvice value attribute is used for basePackages

--- src/main/java/com/school/config/FileUploadExceptionHandler.java
+++ src/main/java/com/school/config/FileUploadExceptionHandler.java
@@ -8,8 +8,8 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 
-@ControllerAdvice(value = "configGlobalExceptionHandler", annotations = Controller.class)
-public class GlobalExceptionHandler {
+@ControllerAdvice(annotations = Controller.class)
+public class FileUploadExceptionHandler {
 
     @ExceptionHandler({org.springframework.web.multipart.MaxUploadSizeExceededException.class, org.springframework.web.multipart.MultipartException.class})
     public String handleMaxSizeException(Exception exc, HttpServletRequest request, org.springframework.web.servlet.mvc.support.RedirectAttributes redirectAttributes) {
